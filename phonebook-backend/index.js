94b9a015b58c7040d22c158cd35c86d78ed79a5e
derek williams <derek61@gmail.com>
2023-10-27 10:20:05 -0600

    add cors middleware, add dist for express

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -1,9 +1,14 @@
 const express = require('express')
 var morgan = require('morgan')
+const cors = require('cors')
+
 
 const app = express()
 
+app.use(express.static('dist'))
+app.use(cors())
 app.use(express.json())
+
 morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body')
