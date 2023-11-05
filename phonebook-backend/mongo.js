69ed7c4e4acafd39ce78e210d07c7e0b46be9031
derek williams <derek61@gmail.com>
2023-11-05 15:50:56 -0700

    completed 3.12: Command-line database

--- phonebook-backend/mongo.js
+++ phonebook-backend/mongo.js
@@ -1,45 +1,66 @@
 const mongoose = require('mongoose')
 
-if (process.argv.length<3) {
-  console.log('give password as argument')
+const logUsage = () => {
+  console.log(`Usage:
+    node mongo.js [password] [new name] [new number]
+  if only password is provided lists the people in the phonebook`)
+}
+
+if (process.argv.length < 3) {
+  logUsage()
+  process.exit(1)
+}
+
+if (process.argv.length > 5) {
+  console.log(`Error, too many arguments`)
+  logUsage()
+  process.exit(1)
+}
+
+if (process.argv.length === 4) {
+  console.log(`Error, both name and number are required`)
+  logUsage()
   process.exit(1)
 }
 
 const password = process.argv[2]
-
-const url =
-  `mongodb+srv://fullstackopenmongo:${password}@freestackopencluster.dhyth5u.mongodb.net/phonebookApp?retryWrites=true&w=majority`
-
-// const url =
-//   `mongodb+srv://fullstackopenmongo:${password}@freestackopencluster.dhyth5u.mongodb.net/noteApp?retryWrites=true&w=majority`
-
-  ///mongodb+srv://<username>:<password>@freestackopencluster.dhyth5u.mongodb.net/?retryWrites=true&w=majority
-
-
-  
-mongoose.set('strictQuery',false)
+const url = `mongodb+srv://fullstackopenmongo:${password}@freestackopencluster.dhyth5u.mongodb.net/phonebookApp?retryWrites=true&w=majority`
+mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
   name: String,
-  number: String,
+  phoneNumber: String,
 })
 
 const Person = mongoose.model('Person', personSchema)
 
-Person.find({}).then(people => {
-  people.forEach(person => {
-    console.log(person)
+const addPerson = (name, phoneNumber) => {
+  const person = new Person({
+    name: name,
+    phoneNumber: phoneNumber,
   })
-  mongoose.connection.close()
-})
 
-// const person = new Person({
-//   name: 'Ringo',
-//   number: '1-2-1234',
-// })
+  person.save().then((person) => {
+    console.log(`added ${person.name} number ${person.phoneNumber} to phonebook`)
+    mongoose.connection.close()
+  })
+}
 
-// person.save().then(result => {
-//   console.log('person saved!')
-//   mongoose.connection.close()
-// })
+const displayPeople = () => {
+  console.log('phonebook:')
+  Person.find({}).then((people) => {
+    people.forEach((person) => {
+      console.log(`${person.name} ${person.phoneNumber}`)
+    })
+    mongoose.connection.close()
+  })
+}
+
+if (process.argv.length === 5) {
+  addPerson(process.argv[3], process.argv[4])
+}
+
+if (process.argv.length === 3) {
+  displayPeople()
+}
